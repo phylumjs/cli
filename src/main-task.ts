7240b176baba9985baa709e5474ebc883930236d
Max J. Polster <max.polster@id-p.de>
2019-04-06 20:02:57 +0200

    Update dependencies.

--- src/main-task.ts
+++ src/main-task.ts
@@ -3,6 +3,6 @@ import { Task } from '@phylum/pipeline';
 import { config } from './config-task';
 
 export default new Task<void>(async task => {
-	const { command, main } = await task.use(config);
+	const { main } = await task.use(config);
 	await task.use(main);
 });
